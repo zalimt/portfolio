b77b3eedf66a9411e4eb6c5e68f6338e8b0150af
Zalim PC <zalim.tsorion@gmail.comC>
2022-07-11 17:51:18 +0300

    header and footer

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -5,7 +5,7 @@
             <router-link class="header" :to="{ name: 'Home' }">FireBlogs</router-link>
         </div>
         <div class="nav-links">
-            <ul>
+            <ul v-show="!mobile">
                 <router-link class="link" to="#">Home</router-link>
                 <router-link class="link" to="#">Blogs</router-link>
                 <router-link class="link" to="#">Create Post</router-link>
@@ -13,9 +13,9 @@
             </ul>
         </div>
     </nav>
-    <menuIcon/>
+    <menuIcon @click="toggleMobileNav" class="menu-icon" v-show="mobile" />
     <transition name="mobile-nav">
-        <ul>
+        <ul class="mobile-nav" v-show="mobileNav">
             <router-link class="link" to="#">Home</router-link>
             <router-link class="link" to="#">Blogs</router-link>
             <router-link class="link" to="#">Create Post</router-link>
@@ -33,6 +33,32 @@ export default {
     components: {
         menuIcon,
     },
+    data() {
+        return {
+            mobile: null,
+            mobileNav: null,
+            windowWidth: null,
+        }
+    },
+    created() {
+        window.addEventListener('resize', this.chechScreen);
+        this.chechScreen();
+    },
+    methods: {
+        chechScreen(){
+            this.windowWidth = window.innerWidth;
+            if (this.windowWidth <= 750){
+                this.mobile = true;
+                return;
+            }
+            this.mobile = false;
+            this.mobileNav = false;
+            return;
+        },
+        toggleMobileNav(){
+            this.mobileNav = !this.mobileNav;
+        }
+    }
 };
 </script>
 
@@ -43,32 +69,91 @@ export default {
         padding: 0 25px;
         box-shadow: 0 4px 6px -1px rgba(0,0,0,0.1), 0 2px 4px -1px rgba(0,0,0,0.06);
         z-index: 99;
-    }
 
-    .link {
-        font-weight: 500;
-        padding: 0 8px;
-        transition: .3s color ease;
-
-        &:hover {
-            color: #1eb8b8;
-        }
-    }
-
-    nav {
-        display: flex;
-        padding: 25px 0;
-
-        .branding {
+        nav {
             display: flex;
-            align-items: center;
-
-            .header {
-                font-weight: 600;
-                font-size: 24px;
-                color: #000;
-                text-decoration: none;
+            padding: 25px 0;
+    
+            .branding {
+                display: flex;
+                align-items: center;
+    
+                .header {
+                    font-weight: 600;
+                    font-size: 24px;
+                    color: #000;
+                    text-decoration: none;
+                }
             }
+            .nav-links {
+                position: relative;
+                display: flex;
+                flex: 1;
+                align-items: center;
+                justify-content: flex-end;
+    
+                ul {
+                    margin-right: 32px;
+    
+                    .link:not(:last-child) {
+                        margin-right: 32px;
+                    }
+                }
+
+                .link {
+                    font-weight: 500;
+                    padding: 0 8px;
+                    transition: .3s color ease;
+
+                    &:hover {
+                        color: #1eb8b8;
+                    }
+                }
+            }
+        }
+
+        .menu-icon {
+            cursor: pointer;
+            position: absolute;
+            top: 30px;
+            right: 25px;
+            height: 25px;
+            width: auto;
+        }
+
+        .mobile-nav {
+            padding: 20px;
+            width: 70%;
+            max-width: 250px;
+            display: flex;
+            flex-direction: column;
+            position: fixed;
+            height: 100%;
+            background: #303030;
+            top: 0;
+            left: 0;
+
+            .link {
+                padding: 15px 0;
+                color: #fff;
+            }
+        }
+
+        .mobile-nav-enter-active,
+        .mobile-nav-leave-active {
+            transition: all 1s ease;
+        }
+
+        .mobile-nav-enter {
+            transform: translateX(-250px);
+        }
+
+        .mobile-nav-enter-to {
+            transform: translateX(0);
+        }
+
+        .mobile-nav-leave-to {
+            transform: translateX(-250px);
         }
     }
 
